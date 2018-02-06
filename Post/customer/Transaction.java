df724f9d51d6a0ede27a2e13ea77b3b61cdc6ef9
Unknown <leibhaftige@hotmail.com>
2018-02-06 13:55:11 -0800

    Finished seperating into packages, and implementing Transaction
    Transaction and its subclasses depend on the payment class and its subclasses, and those are only really used by customer, so I separated them into their own package

---
+++ Post/customer/Transaction.java
@@ -0,0 +1,16 @@
+package customer;
+
+public class Transaction {
+    
+    public void setHeader(TransactionHeader header){
+        
+    }
+    
+    public void setPayment(Payment payment){
+        
+    }
+    
+    public void addTransactionItem(TransactionItem item){
+        
+    }
+}
---
+++ Post/customer/TransactionHeader.java
@@ -0,0 +1,14 @@
+package customer;
+
+public class TransactionHeader {
+    String customerName;
+    java.util.Date date;
+    
+    TransactionHeader(String name, java.util.Date date){
+        this.customerName = name;
+        this.date = date;        
+    }
+    
+    // toString();
+    
+}
